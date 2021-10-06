a05e6e5e5c1e9123453197448c8ce76415d9ce11
Dimahtml <bomabo@mail.ru>
2021-10-06 11:33:46 +0500

    3.1. Добавит маршрутизацию.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import MainPageScreen from '../main-page-screen/main-page-screen';
+import AddReviewScreen from '../add-review-screen/add-review-screen';
+import MovieScreen from '../movie-screen/movie-screen';
+import MyListScreen from '../my-list-screen/my-list-screen';
+import PlayerScreen from '../player-screen/player-screen';
+import SignInScreen from '../sign-in-screen/sign-in-screen';
 
 type AppProps = {
   name: string,
@@ -7,7 +14,31 @@ type AppProps = {
 }
 
 function App({name, genre, released}: AppProps): JSX.Element {
-  return <MainPageScreen name={name} genre={genre} released={released} />;
+  // return <MainPageScreen name={name} genre={genre} released={released} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.MainPage}>
+          <MainPageScreen name={name} genre={genre} released={released} />
+        </Route>
+        <Route exact path={AppRoute.AddReview}>
+          <AddReviewScreen />
+        </Route>
+        <Route exact path={AppRoute.Film}>
+          <MovieScreen />
+        </Route>
+        <Route exact path={AppRoute.MyList}>
+          <MyListScreen />
+        </Route>
+        <Route exact path={AppRoute.Player}>
+          <PlayerScreen />
+        </Route>
+        <Route exact path={AppRoute.SignIn}>
+          <SignInScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
