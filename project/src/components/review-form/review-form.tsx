cfa98d4c8908037d25283aa1e26c70ffe3117d88
Dimahtml <bomabo@mail.ru>
2021-10-21 19:29:10 +0500

    поднятие состояния из инпутов в review-form

--- project/src/components/review-form/review-form.tsx
+++ project/src/components/review-form/review-form.tsx
@@ -3,16 +3,23 @@ import RatingInputs from '../rating-inputs/rating-inputs';
 
 function ReviewForm(): JSX.Element {
   const [message, setMessage] = useState('');
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [rating, setRating] = useState(0);
 
   return (
     <div className="add-review">
       <form action="#" className="add-review__form">
         <div className="rating">
-          <RatingInputs />
+          <RatingInputs
+            onRatingChange={setRating}
+          />
         </div>
 
         <div className="add-review__text">
-          <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"
+          <textarea
+            className="add-review__textarea"
+            name="review-text" id="review-text"
+            placeholder="Review text"
             onChange={( {target}: ChangeEvent<HTMLTextAreaElement>) => {
               setMessage(target.value);
             }}
