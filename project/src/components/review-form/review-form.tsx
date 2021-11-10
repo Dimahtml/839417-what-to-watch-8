35559f3f4d3abde3e5281c6b6848f07ce0aa8f3b
Dimahtml <bomabo@mail.ru>
2021-11-10 13:18:33 +0500

    валидация формы

--- project/src/components/review-form/review-form.tsx
+++ project/src/components/review-form/review-form.tsx
@@ -1,4 +1,6 @@
-import {useState, ChangeEvent, FormEvent} from 'react';
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {useState, useEffect, ChangeEvent, FormEvent} from 'react';
 import RatingInputs from '../rating-inputs/rating-inputs';
 import {AddReview} from '../../types/add-review';
 
@@ -9,18 +11,43 @@ type ReviewFormProps = {
 function ReviewForm(props: ReviewFormProps): JSX.Element {
   const onSubmit = props.onSubmit;
   const [message, setMessage] = useState('');
-  // этот disable уберу, когда нужно будет использовать rating
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [rating, setRating] = useState(0);
+  const [messageDirty, setMessageDirty] = useState(false);
+  const [messageError, setMessageError] = useState('Message can`t be empty');
+  const [formValid, setFormValid] = useState(false);
+
+  useEffect(() => {
+    if (messageError || rating === 0) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [messageError, rating]);
 
   const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
-
-    if (rating !== undefined && message) {
+    if (rating !== undefined && messageError === '') {
       onSubmit({
         rating: rating,
         comment: message,
       });
+    }
+  };
+
+  const messageHandler = (evt: any) => {
+    setMessage(evt.target.value);
+    if ((message.length < 50) || (message.length > 400)) {
+      setMessageError('Valid message is from 50 to 400 characters');
+    } else {
+      setMessageError('');
+    }
+  };
+
+  const blurHandler = (evt: any) => {
+    switch (evt.target.name) {
+      case 'review-text':
+        setMessageDirty(true);
+        break;
     }
   };
 
@@ -34,18 +61,22 @@ function ReviewForm(props: ReviewFormProps): JSX.Element {
         </div>
 
         <div className="add-review__text">
+          {
+            (messageDirty && messageError)
+              ? <div style={{color:'red'}}>{messageError}</div>
+              : ''
+          }
           <textarea
             className="add-review__textarea"
             name="review-text" id="review-text"
             placeholder="Review text"
-            onChange={( {target}: ChangeEvent<HTMLTextAreaElement>) => {
-              setMessage(target.value);
-            }}
+            onChange={(evt) => messageHandler(evt)}
+            onBlur={(evt) => blurHandler(evt)}
             value={message}
           >
           </textarea>
           <div className="add-review__submit">
-            <button className="add-review__btn" type="submit">Post</button>
+            <button className="add-review__btn" type="submit" disabled={!formValid}>Post</button>
           </div>
 
         </div>
