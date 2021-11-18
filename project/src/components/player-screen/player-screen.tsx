a3184e9a40d01f8ca19362e5a655fda725f815f3
Dimahtml <bomabo@mail.ru>
2021-11-18 13:05:48 +0500

    плеер. прогресс бар. оставшееся время

--- project/src/components/player-screen/player-screen.tsx
+++ project/src/components/player-screen/player-screen.tsx
@@ -1,8 +1,6 @@
-/* eslint-disable no-console */
-import {useState} from 'react';
+import {useState, useRef} from 'react';
 import {useParams, useHistory} from 'react-router-dom';
 import {Film, Films} from '../../types/film';
-import VideoPlayer from '../video-player/video-player';
 import PlayButton from '../play-button/play-button';
 import PauseButton from '../pause-button/pause-button';
 
@@ -15,6 +13,10 @@ function PlayerScreen({films}: PlayerScreenProps): JSX.Element {
   const {id} = useParams<{id?: string}>();
   const film = films.find((filmItem) => filmItem.id === Number(id)) || {} as Film;
   const [isActive, setActive] = useState(false);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [percent, setPercent] = useState(0);
+  let video = videoRef.current;
 
   function onExitButtonClick() {
     history.goBack();
@@ -22,23 +24,64 @@ function PlayerScreen({films}: PlayerScreenProps): JSX.Element {
 
   function onPlayButtonClick() {
     if (isActive) {
+      videoRef.current?.pause();
       setActive(false);
     } else {
+      videoRef.current?.play();
       setActive(true);
     }
+  }
+
+  function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+    }
+  }
+
+  function getRemainingTime(percents: number, durationInSec: any) {
+    let result = '';
+    durationInSec = Math.floor(durationInSec * (100 - percents) / 100);
+    if (durationInSec < 10) {
+      durationInSec = `0${durationInSec}`;
+    }
+    let durationInMin: number | string = Math.floor(durationInSec / 60);
+    if (durationInMin < 10) {
+      durationInMin = `0${durationInMin}`;
+    }
+    result = `-${durationInMin}:${durationInSec}`;
+    return result;
   }
 
   return (
     <div className="player">
 
-      <VideoPlayer
+      <video
+        ref={videoRef}
         className="player__video"
-        isPlaying={isActive}
         src={film.previewVideoLink}
         poster={film.backgroundImage}
         width="100%"
         height="100%"
         muted={false}
+        onTimeUpdate={() => {
+          video = videoRef.current;
+
+          if (!video) {
+            return;
+          }
+
+          if(!isNaN(video.duration)) {
+            const currentPercent = video.currentTime / video.duration * 100;
+            setPercent(currentPercent);
+          }
+        }}
+        onLoadedData={() => {
+          setIsLoading(false);
+        }}
       />
 
       <button type="button" className="player__exit" onClick={onExitButtonClick}>Exit</button>
@@ -46,10 +89,12 @@ function PlayerScreen({films}: PlayerScreenProps): JSX.Element {
       <div className="player__controls">
         <div className="player__controls-row">
           <div className="player__time">
-            <progress className="player__progress" value="30" max="100"></progress>
-            <div className="player__toggler" style={{left: '30%'}}>Toggler</div>
+            <progress className="player__progress" value={percent} max="100"></progress>
+            <div className="player__toggler" style={{left: `${percent}%`}}>Toggler</div>
           </div>
-          <div className="player__time-value">1:30:29</div>
+          <div className="player__time-value">
+            {isLoading ? 'loading...' : getRemainingTime(percent, video!.duration)}
+          </div>
         </div>
 
         <div className="player__controls-row">
@@ -62,7 +107,11 @@ function PlayerScreen({films}: PlayerScreenProps): JSX.Element {
           </button>
           <div className="player__name">Transpotting</div>
 
-          <button type="button" className="player__full-screen">
+          <button
+            type="button"
+            className="player__full-screen"
+            onClick={toggleFullScreen}
+          >
             <svg viewBox="0 0 27 27" width="27" height="27">
               <use xlinkHref="#full-screen"></use>
             </svg>
